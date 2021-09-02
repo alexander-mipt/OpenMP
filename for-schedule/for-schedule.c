31bbb7a2b7b4b5528081d0eee5f92f65e934f221
alexander-mipt <antipov.ae@phystech.edu>
2021-09-03 01:46:51 +0300

    for-schedule: working prtototype, all functions were realized

--- for-schedule/for-schedule.c
+++ for-schedule/for-schedule.c
@@ -1,31 +1,48 @@
-#include<stdio.h>
-#include<omp.h>
-#include<stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <stdint.h>
+#include "assert.h"
 
-void init_omp() {
+void init_omp(int threads_num) {
     // omp_set_dynamic(0);     // Explicitly disable dynamic teams
-    omp_set_num_threads(4); // Use 4 threads for all consecutive parallel regions
+    omp_set_num_threads(10); // Use 4 threads for all consecutive parallel regions
 }
 
-void info() {
-    printf("threads num max: %d\n", omp_get_max_threads());
+void info(uint64_t max) {
+    printf("threads num: %d\n", omp_get_num_threads());
+    printf("count up to N]: %lu\n", max);
 }
 
-int main() {
-    // init_omp();
-    info();
+int main(int argc, char* argv[]) {
+    assert(argc <= 3);
 
+    uint64_t max_num = 20;
     uint64_t sum = 0;
-    
-#pragma omp parallel
-{
-    #pragma omp for reduction(+:sum)
-    for(uint64_t i = 1; i <= 10; i++) {
-        printf("t: %d, %lu\n", omp_get_thread_num(), i);
-        sum += i;
+
+    if (argc > 1) {
+        int threads_num = atoi(argv[1]);
+        assert(threads_num > 0);
+        omp_set_num_threads(threads_num);
     }
-    printf("%lu\n", sum);
-}
+
+    if (argc == 3) {
+        max_num = atoi(argv[2]);
+        assert(max_num <= 10000);
+    }
+
+    info(max_num);
+    
+    #pragma omp parallel
+    {
+        #pragma omp for reduction(+:sum) schedule(static)
+        for(uint64_t i = 1; i <= max_num; i++) {
+            printf("t: %d, %lu\n", omp_get_thread_num(), i);
+            sum += i;
+        }
+    }
+
+    printf("total sum: %lu\n", sum);
 }
 
 // we can't use shared(...) with #pragma omp for
